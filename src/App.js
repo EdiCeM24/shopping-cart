d673b1374b70e8f6c177e23135cbdf0a792b32d1
Marcus Edidiong Cletus <edi_mars@outlook.com>
2023-01-09 08:55:01 -0800

    instll reactjs shopping-cart node_modules public src and create template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Basket from './components/Basket';
 import Header from './components/Header';
 import Main from './components/Main';
+import Basket from './components/Basket';
 import data from './data';
 
 function App() {
