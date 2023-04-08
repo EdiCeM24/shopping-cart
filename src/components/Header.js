abab451b2b653e5ba7f7aa8fcd174b3f827fc2b6
Marcus Edidiong Cletus <edi_mars@outlook.com>
2023-04-07 23:16:23 -0700

    commit all changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ export default function Header(props) {
     return (
        <div className="row center block">
            <div>
+               <a href="#/"><h1>Edi_CeM24</h1></a>
               <a href="#/">
                  <h2>Shopping Cart</h2>
                </a>
